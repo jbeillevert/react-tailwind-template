3f6f849aa5a5d286890457111aad4a5c174d2159
Jean-Emmanuel Beillevert <beillevertj.contact@gmail.com>
2023-06-17 15:38:54 +0200

    initialisation template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+
+  return (
+    <div className="bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 h-screen flex items-center justify-center flex-col">
+      <h1 className="text-slate-950 text-4xl font-bold">Happy coding avec VRT</h1>
+      <p className="text-slate-700">Le template Vite-React-Tailwind</p>
+    </div>
+  )
+}
+
+export default App
