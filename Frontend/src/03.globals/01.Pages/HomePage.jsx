942a6d3166de89e1997502253e50a16703b25323
J-E <116655199+jbeillevert@users.noreply.github.com>
2023-07-25 13:33:48 +0200

    Update HomePage.jsx

--- Frontend/src/03.globals/01.Pages/HomePage.jsx
+++ Frontend/src/03.globals/01.Pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navbar from '../03.Components/Navbar';
+import Hero from '../02.Sections/Hero';
 
 const HomePage = () => {
     return (
-        <div>
-            
+        <div classname="h-full w-full max-w-7xl p-6 flex flex-col gap-40">
+            <Navbar />
+            <Hero />
         </div>
     );
 };
