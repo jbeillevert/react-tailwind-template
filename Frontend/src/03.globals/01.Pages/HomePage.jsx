632c3148d26a63ce4355cb29560e048f018260ed
J-E <116655199+jbeillevert@users.noreply.github.com>
2024-01-08 11:41:34 +0100

    Update HomePage.jsx camelcase correction

--- Frontend/src/03.globals/01.Pages/HomePage.jsx
+++ Frontend/src/03.globals/01.Pages/HomePage.jsx
@@ -4,7 +4,7 @@ import Hero from '../02.Sections/Hero';
 
 const HomePage = () => {
     return (
-        <div classname="h-full w-full max-w-7xl p-6 flex flex-col gap-40">
+        <div className="h-full w-full max-w-7xl p-6 flex flex-col gap-40">
             <Navbar />
             <Hero />
         </div>
